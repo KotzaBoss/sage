a8a073d073fa7574b68e02cf6a4f9700e324ded8
Georgios KOTZAMPOPOULOS <georgios.kotzampopoulos@tutanota.com>
2023-07-25 14:30:12 +0200

    Add [...] around log type in spdlog pattern, simple log test

---
+++ test/log.test.cpp
@@ -0,0 +1,15 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+#include "log.hpp"
+
+using namespace sage;
+
+TEST_CASE ("Log") {
+	SAGE_LOG_TRACE("This is some: TRACE");
+	SAGE_LOG_DEBUG("This is some: DEBUG");
+	SAGE_LOG_INFO("This is some: INFO");
+	SAGE_LOG_WARN("This is some: WARN");
+	SAGE_LOG_ERROR("This is some: ERROR");
+	SAGE_LOG_CRITICAL("This is some: CRITICAL");
+}
