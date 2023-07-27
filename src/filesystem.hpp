650267664ffc68067332a77a4e01935507119d1f
Georgios KOTZAMPOPOULOS <georgios.kotzampopoulos@tutanota.com>
2023-07-27 14:06:01 +0200

    Add ios::in mask for ifstream in read_file

--- src/filesystem.hpp
+++ src/filesystem.hpp
@@ -9,7 +9,7 @@ namespace sage::inline filesystem {
 inline auto read_file(const fs::path& p) -> std::string {
 	SAGE_ASSERT_PATH_READABLE(p);
 
-	auto stream = std::ifstream{p, std::ios::binary};
+	auto stream = std::ifstream{p, std::ios::in | std::ios::binary};
 	SAGE_ASSERT(stream);
 
 	auto content = std::string{};
