a10ff08ebf21d9c3635050414114c61b97ca2d84
Georgios KOTZAMPOPOULOS <georgios.kotzampopoulos@tutanota.com>
2023-08-03 01:48:45 +0200

    Add more space for function name on spdlog message format

--- src/log.cpp
+++ src/log.cpp
@@ -9,7 +9,7 @@ inline const Log Log::log = Log();
 
 Log::Log() {
 	// [log_type] (time) ~thread logger file:line:function: message
-	spdlog::set_pattern("%^[%-8l] (%T) ~%t %n %s:%#::%-10!!: %v%$");
+	spdlog::set_pattern("%^[%-8l] (%T) ~%t %n %s:%#::%-15!!: %v%$");
 #ifdef NDEBUG
 	spdlog::set_level(spdlog::level::info);
 #else
