9cda166f30e009c1ae5966827b23911da8758bbd
Georgios KOTZAMPOPOULOS <georgios.kotzampopoulos@tutanota.com>
2023-10-05 10:15:25 +0200

    Fix spdlog level

--- src/log.cpp
+++ src/log.cpp
@@ -11,9 +11,9 @@ Log::Log() {
 	// [log_type] (time) ~thread logger file:line:function: message
 	spdlog::set_pattern("%^[%-8l] (%T) ~%t %n %s:%#::%-20!!: %v%$");
 	if constexpr (build::in_debug_mode)
-		spdlog::set_level(spdlog::level::info);
-	else
 		spdlog::set_level(spdlog::level::trace);
+	else
+		spdlog::set_level(spdlog::level::info);
 
 	SAGE_LOG_INFO("Logging initialized");
 }
