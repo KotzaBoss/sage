934b2021e1ca6f0c1d31ebfbace3a68a5f5889b1
Georgios KOTZAMPOPOULOS <georgios.kotzampopoulos@tutanota.com>
2023-12-26 16:43:21 +0100

    Add delta_coefficient and rename Tick member

--- src/time.hpp
+++ src/time.hpp
@@ -12,31 +12,32 @@ concept Duration = type::Any<D,
 	>
 	;
 
-template<Number Rep> using microseconds = std::chrono::duration<Rep, std::micro>;
-template<Number Rep> using milliseconds = std::chrono::duration<Rep, std::milli>;
+constexpr auto delta_coefficient(const Duration auto dt) -> float {
+	return std::chrono::duration<float, std::chrono::seconds::period>{dt}.count();
+}
 
 template<Duration D = std::chrono::milliseconds>
 struct Tick {
 	using Duration = D;
 	using Clock = std::chrono::steady_clock;
 
-private:
-	Clock::time_point prev;
+protected:
+	Clock::time_point current;
 
 public:
 	Tick()
-		: prev{Clock::now()}
+		: current{Clock::now()}
 	{}
 
 public:
 	// Tick and return elapsed time since previous tick
 	auto operator() () -> Duration {
 		const auto now = Clock::now();
-		return std::chrono::duration_cast<Duration>(now - std::exchange(prev, now));
+		return std::chrono::duration_cast<Duration>(now - std::exchange(current, now));
 	}
 
 	auto current_time_point() const -> Clock::time_point {
-		return prev;
+		return current;
 	}
 };
 
